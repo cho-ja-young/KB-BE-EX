0d232f8661a189b669c6e15b522eb29b20c8e2cc
cho-ja-young <26006@naver.com>
2025-04-15 16:41:20 +0900

    Add : 20250414 basic, hard code add

---
+++ Study/src/ch02/sec01/VariableInitializationExample.java
@@ -0,0 +1,12 @@
+//package ch02;
+//
+//public class VariableInitializationExample {
+//    public static void main(String[] args) {
+//        int value;
+//        
+//        int result = value + 10;
+//        
+//        System.out.println(result);
+//    }
+//}
+// 문제점 : 변수 value 를 초기화하지 않았다
