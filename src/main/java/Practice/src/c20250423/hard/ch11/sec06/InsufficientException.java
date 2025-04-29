8bbc71ef08a9e3a7aa2ecac06fdca778b53df944
cho-ja-young <26006@naver.com>
2025-04-29 16:48:23 +0900

    Add : 20250423 practice hard01 code add

---
+++ src/main/java/Practice/src/c20250423/hard/ch11/sec06/InsufficientException.java
@@ -0,0 +1,9 @@
+package Practice.src.c20250423.hard.ch11.sec06;
+
+public class InsufficientException extends Exception {
+    public InsufficientException() {}
+
+    public InsufficientException(String message) {
+        super(message);
+    }
+}
