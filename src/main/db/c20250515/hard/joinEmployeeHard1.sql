05aec95575f313da5b95efad93d15cdfc366f2d9
cho-ja-young <26006@naver.com>
2025-05-15 17:48:31 +0900

    Add : 20250515 db practice hard FIX

--- src/main/db/c20250515/hard/joinEmployeeHard1.sql
+++ src/main/db/c20250515/hard/joinEmployeeHard1.sql
@@ -12,7 +12,16 @@ WHERE T.to_date = '9999-01-01';
 
 SELECT E.emp_no, E.birth_date, E.first_name, E.last_name, E.gender, E.hire_date, T.title, S.salary
 FROM employees E
-LEFT OUTER JOIN titles T ON E.emp_no = T.emp_no
-LEFT OUTER JOIN salaries S ON T.emp_no = S.emp_no
-WHERE T.to_date = '9999-01-01' AND S.to_date = '9999-01-01';
+INNER JOIN titles T ON E.emp_no = T.emp_no AND T.to_date = '9999-01-01'
+INNER JOIN salaries S ON T.emp_no = S.emp_no AND S.to_date = '9999-01-01';
 
+
+-- 현재 재직 중인 직원의 정보를 출력
+-- 출력항목 : emp_no, first_name, last_name, department
+-- 정렬 : emp_no 오름차순
+
+SELECT E.emp_no, E.first_name, E.last_name, D.dept_name
+FROM employees E
+         INNER JOIN dept_emp DE ON E.emp_no = DE.emp_no AND DE.to_date = '9999-01-01'
+         INNER JOIN departments D ON DE.dept_no = D.dept_no
+ORDER BY E.emp_no;
